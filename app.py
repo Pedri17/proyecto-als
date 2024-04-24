4d7f01ec822ffd82e35e83adf3188a66ac724bde
Pedri17 <pedrojalda@gmail.com>
2024-04-24 19:36:29 +0200

    Page base

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-print("Hello world")
+import flask
+
+app = flask.Flask(__name__)
+
+
+@app.route("/")
+def get_index():
+    return flask.send_from_directory(app.static_folder, "index.html")
+
+
+@app.route("/hi", methods=["POST"])
+def post_answer():
+    nombre = flask.request.form["nombre1"]
+    values = {
+        "nombre_persona": nombre
+    }
+    return flask.render_template("response.html", **values)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
